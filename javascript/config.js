2b112079852da212c971a9e1e53df750b7e6a1b2
nkina <153473211+Consolate25@users.noreply.github.com>
2024-04-14 21:36:28 -0400

    first commit

---
+++ javascript/config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlqnVC_1LLfVJdAijZDP8axj1AD6D11xg",
+    authDomain: "final1-5f532.firebaseapp.com",
+    projectId: "final1-5f532",
+    storageBucket: "final1-5f532.appspot.com",
+    messagingSenderId: "997126978160",
+    appId: "1:997126978160:web:9cd4bd5a943b5011ca92b5"
+  };
+  
+      // Initialize Firebase
+    
+    firebase.initializeApp(firebaseConfig)
+    firebase.analytics()
